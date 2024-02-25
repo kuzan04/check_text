8c690253135cf4bf55ae3123048a4dad42f305cc
kuzan04 <kittiphum1230@hotmail.co.th>
2024-02-25 19:21:01 +0700

    Modify App to Version1.1.0

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
+extern  crate winres;
+
 fn main() {
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("check-1292819_640.ico");
+        res.compile().unwrap();
+    }
+
     slint_build::compile("ui/appwindow.slint").unwrap();
 }
